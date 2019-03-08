7233802bc0446233fbddb05facb5ddcac67a5b7e
Lucas Almeida Salvador <37023076+LASalvador@users.noreply.github.com>
2019-03-08 10:26:35 -0300

    feature -  adicionando metodo para criar eventos

--- quickstart.py
+++ quickstart.py
@@ -7,7 +7,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES1 = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES2 = ['https://www.googleapis.com/auth/calendar']
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -27,7 +28,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'credentials.json', SCOPES2)
             creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
@@ -49,21 +50,39 @@ def main():
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
-    # Refer to the Python quickstart on how to setup the environment:
-    # https://developers.google.com/calendar/quickstart/python
-    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-    # stored credentials.
+
+def criaEvento():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES2)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
 
     event = {
       'summary': 'Google I/O 2015',
       'location': '800 Howard St., San Francisco, CA 94103',
       'description': 'A chance to hear more about Google\'s developer products.',
       'start': {
-        'dateTime': '2019-05-28T09:00:00-07:00',
+        'dateTime': '2015-05-28T09:00:00-07:00',
         'timeZone': 'America/Los_Angeles',
       },
       'end': {
-        'dateTime': '2019-05-28T17:00:00-07:00',
+        'dateTime': '2015-05-28T17:00:00-07:00',
         'timeZone': 'America/Los_Angeles',
       },
       'recurrence': [
@@ -82,10 +101,8 @@ def main():
       },
     }
 
-    #event = service.events().insert(calendarId='primary', body=event).execute()
-    link_event = event.get('htmlLink')
-    print(f'Event created:{link_event}')
-    print('[main - FIM]')
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    link = event.get('htmlLink')
+    print(f'Event created: {link}')
+    
 
-if __name__ == '__main__':
-    main()
