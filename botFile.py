601c4f3ae1cebe672c59d8d581f04f69a8ed7b5d
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2019-05-10 10:16:19 -0300

    fix - adicionando robo de teste

--- botFile.py
+++ botFile.py
@@ -1,6 +1,13 @@
-import telegram
+# https://github.com/python-telegram-bot/python-telegram-bot/blob/2cde878d1e5e0bb552aaf41d5ab5df695ec4addb/examples/timerbot.py
 
 from telegram.ext import Updater, CommandHandler
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 def echo(bot, chat, mensagem):
     bot.sendMessage(chat_id=chat, text = mensagem)
@@ -9,35 +16,12 @@ bot = telegram.Bot(token = '625881619:AAFkDG8Fw8btJxVzWyz-GCru3nrT7gXYvfc')
 echo(bot, '798821507', 'Ola Lucas, o que deseja fazer: \n 1 - Consultar eventos \n 2 - Editar eventos\n 3 - Apagar eventos\n 4 - Criar eventos')
 
 # @botMensagem.message_handler(func=lambda m: True)
+def start(bot, update):
+    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
 
-def start(update, context):
-	print("RODOUUU RODADO IMPO")
-	update.message.reply_text('Funciona')
-	job = context.job
-	context.bot.send_message(job.context, text='Beep!')
-
-def main():
-    """Run bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater('625881619:AAFkDG8Fw8btJxVzWyz-GCru3nrT7gXYvfc',user_sig_handler=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-
-    # dp.add_error_handler(error)
-
-    updater.start_polling()
-    print("RODOUUU RODADO")
-
-    updater.idle()
 
 
 # botMensagem.polling()
 
 if __name__ == '__main__':
-	main()
+	main()	
