b3fdeff45f4b870029048698f591c583aa664a17
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-06-01 11:48:39 -0300

    fix - atualizando estrutura do bot

--- botTest.py
+++ botTest.py
@@ -10,83 +10,82 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+MAPEAR_ACAO, CRIAR, CONSULTAR, ALTERAR, DELETAR, CANCELAR = range(6)
 
 
 def start(bot, update):
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    reply_keyboard = [['Criar', 'Consultar','Alterar', 'Deletar']]
 
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
+        'Olá! Eu sou o Schedule Bot.Vim ajudar-te a controlar sua agenda.\n\n'
+        'O que deseja fazer',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    return GENDER
+    return ACAO
 
 
-def gender(bot, update):
+def acao(bot, update):
+    # user = update.message.from_user
+    # logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    # update.message.reply_text('I see! Please send me a photo of yourself, '
+    #                           'so I know what you look like, or send /skip if you don\'t want to.',
+    #                           reply_markup=ReplyKeyboardRemove())
+    acao = update.message.text
+
+    if(acao == 'Criar'):
+        update.message.reply_text('Digite as informações do evento que deseja criar',
+                               reply_markup=ReplyKeyboardRemove())
+        return CRIAR
+    elif (acao == 'Consultar'):
+        update.message.reply_text('Digite as informações do evento que deseja consultar',
+                               reply_markup=ReplyKeyboardRemove())
+        return CONSULTAR
+    elif (acao == 'Alterar'):
+        update.message.reply_text('Digite as informações do evento que deseja alterar',
+                               reply_markup=ReplyKeyboardRemove())
+        return ALTERAR
+    elif (acao == 'Deletar'):
+        update.message.reply_text('Digite as informações do evento que deseja deletar',
+                               reply_markup=ReplyKeyboardRemove())
+        return DELETAR
+
+    return CANCELAR
+
+
+
+def criar(bot, update):
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('I see! Please send me a photo of yourself, '
-                              'so I know what you look like, or send /skip if you don\'t want to.',
-                              reply_markup=ReplyKeyboardRemove())
-
-    return PHOTO
-
-
-def photo(bot, update):
-    user = update.message.from_user
-    photo_file = bot.get_file(update.message.photo[-1].file_id)
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text('Gorgeous! Now, send me your location please, '
-                              'or send /skip if you don\'t want to.')
-
-    return LOCATION
-
-
-def skip_photo(bot, update):
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text('I bet you look great! Now, send me your location please, '
-                              'or send /skip.')
-
-    return LOCATION
-
-
-def location(bot, update):
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                user_location.longitude)
-    update.message.reply_text('Maybe I can visit you sometime! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
-
-
-def skip_location(bot, update):
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text('You seem a bit paranoid! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
-
-
-def bio(bot, update):
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
+    logger.info("Criar %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('O usuário deseja criar um evento.')
 
     return ConversationHandler.END
 
+def consultar(bot, update):
+    user = update.message.from_user
+    logger.info("Consultar %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('O usuário deseja consultar um evento.')
 
+    return ConversationHandler.END
+
+def alterar(bot, update):
+    user = update.message.from_user
+    logger.info("Criar %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('O usuário deseja alterar um evento.')
+
+    return ConversationHandler.END
+
+def deletar(bot, update):
+    user = update.message.from_user
+    logger.info("Criar %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('O usuário deseja deletar um evento.')
+
+    return ConversationHandler.END
+
+    
 def cancel(bot, update):
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
+    update.message.reply_text('Tchau! Espero ter ajudado.',
                               reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
@@ -104,20 +103,26 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+
+    MAPEAR_ACAO, CRIAR, CONSULTAR, ALTERAR, DELETAR, CANCELAR = range(6)
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
-            GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
+            MAPEAR_ACAO: [RegexHandler('^(Criar|Consultar|Alterar|Deletar)$', acao)],
 
-            PHOTO: [MessageHandler(Filters.photo, photo),
-                    CommandHandler('skip', skip_photo)],
+            CRIAR: [MessageHandler(Filters.text, criar),
+                    CommandHandler('cancel', cancel)],
 
-            LOCATION: [MessageHandler(Filters.location, location),
-                       CommandHandler('skip', skip_location)],
+            CONSULTAR: [MessageHandler(Filters.text, consultar),
+                    CommandHandler('cancel', cancel)],
 
-            BIO: [MessageHandler(Filters.text, bio)]
+            ALTERAR: [MessageHandler(Filters.text, alterar),
+                    CommandHandler('cancel', cancel)],
+
+            DELETAR: [MessageHandler(Filters.text, deletar),
+                    CommandHandler('cancel', cancel)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
