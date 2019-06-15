e268b2bcc6ba34eaed8809bef811a93bd3d2a0ab
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-06-15 16:22:34 -0300

    atualizando botfile

--- botFile.py
+++ botFile.py
@@ -105,7 +105,7 @@ def error(bot, update, error):
 
 def main():
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("625881619:AAFkDG8Fw8btJxVzWyz-GCru3nrT7gXYvfc")
+    updater = Updater("")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
